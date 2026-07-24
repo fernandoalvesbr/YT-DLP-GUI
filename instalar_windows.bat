11bd5f787f0187fe0234c0a1fe917586515ed7d5
Fernando Alves <100968836+fernandoalvesbr@users.noreply.github.com>
2026-07-24 16:34:29 -0300

    Delete instalar_windows.bat

--- instalar_windows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Instalador - YT-DLP GUI
-cd /d "%~dp0"
-python -m pip install -U pip
-python -m pip install -U -r requirements.txt
-echo.
-echo Instalacao concluida.
-echo Para abrir, execute iniciar_windows.bat
-pause
